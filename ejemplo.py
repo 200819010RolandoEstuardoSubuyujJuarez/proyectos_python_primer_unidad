eeed50c1b84efd727921d02a7cbc1c44d79f591d
200819010RolandoEstuardoSubuyujJuarez <2558610600101@ingenieria.usac.edu.gt>
2020-06-05 18:31:19 -0600

    segunda actuarlizacion

--- ejemplo.py
+++ ejemplo.py
@@ -5,11 +5,15 @@ print('''este es tu primer programa
 print("\n\n\n\n")
 
 
-
-
 print("hola mundo")
 
-
+a = input("ingrese algo: ")
+b =a.isdigit()
+print(type(b))
+if b == True:
+    print("se 1ingreso un digito")
+else:
+    print("no es un digito")
 
 
 print("\n\n\n\n")
